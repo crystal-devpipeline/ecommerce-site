e83a40514949f7f5e37a4207b116afb5c96cda2a
Crystal Sorensen <crystal@devpipeline.com>
2023-01-02 21:36:21 -0700

    Updated About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,8 +6,7 @@ export default function About() {
             It's ABOUT time!!!
         </h1>
         <p>
-            Welcome to Crystolivia, Where you can buy anything your heart desires... on this one page!
-            Feel free to browse the product page, look at an individual item, and even add that item to your cart! It has a fully fuctional navbar for you to navigate to different pages. Fill out the form on the contact page so we can start sending you atleast 10 emails a day. We'd love to hear from you!  
+            This Capstone is an ecommerce site that I built to use the skills I learned in the frontend course. The elements of this web application were designed to utilize specific parts of React for practice and learning purposes. When you click through you will be able to see fetch functionality and cart functionality. I accessed an API to get all of the data for the products. I also used react-router-dom v5 to route between pages. I found many opprotunities to use state and props as well, which made building this application much easier than vanilla js. I hope you like it Ryan lol.
         </p>
         <p>
             Now, to explain what I've learned through my time here... Boy oh boy, where do I begin? First of all, FOCUS! Focus and dicipline are two of the most important things I've realized are key to software development. I have been working on my focus and study habits to stay on top of the fire hose of information blasting into my brain over the last few months. It is challenging and hard. One of my mottos in life is "Hard things make you Great!". The only problem is my other motto... "The harder you play, the younger you stay!" So, balancing "fun" and "hard mental work" can be difficult. Another thing I've realized is that I need to allow myself the grace to fail and receive the feedback that comes with it. I get behind and feel embarrassed to admit that I don't know what I'm doing, so I hold back and get frustrated and lost. Then I get so far behind that I get overwhelmed and don't know where to start. These things have been teaching me a lot about myself and what I need to do to find the success that I long for. One thing I do love are my "Ah ha!" moments. They are extremely rewarding when things finally Click! Piece by piece it keeps nudging me onward.
@@ -15,7 +14,7 @@ export default function About() {
         </p>
         <p>
             My Favorite Coding Language...
-            All of the different languages have their pros and cons for now. I am a very visual learner so that makes the easy ones like HTML and CSS my favorite for now because I can see immediate results and they are easy to figure out and rewarding to work with. I know that once I understand React it will be my favorite. I'm intrigued in discovering how all the moving components and peices unveil the magic of the INTERWEB! 
+            All of the different languages have their pros and cons for now.  I know that once I understand React it will be my favorite. I'm intrigued in discovering how all the moving components and pieces unveil the magic of the INTERWEB! 
         </p>
     </div>
   );
